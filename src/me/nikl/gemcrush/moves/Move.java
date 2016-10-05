b168c14f619da4b2e8d9289c35d943a5caf18653
BuildTools <unconfigured@null.spigotmc.org>
2016-10-05 23:09:18 +0400

    experimental way to recognise possible moves

---
+++ src/me/nikl/gemcrush/moves/Move.java
@@ -0,0 +1,9 @@
+package me.nikl.gemcrush.moves;
+
+/**
+ * Created by niklas on 10/3/16.
+ */
+public interface Move {
+	public void action();
+	
+}
---
+++ src/me/nikl/gemcrush/moves/ThreeInRow.java
@@ -0,0 +1,8 @@
+package me.nikl.gemcrush.moves;
+
+/**
+ * Created by niklas on 10/3/16.
+ */
+public class ThreeInRow implements Move {
+	
+}
